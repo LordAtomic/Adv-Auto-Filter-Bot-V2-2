b87abac7459e438c4226f9cd10b190ae04c416eb
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-05-26 23:41:42 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -162,7 +162,7 @@ async def auto_filter(bot, update):
             InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
         ])
         
-        
+        result[0].append([ InlineKeyboardButton(f"🔰 Channel🔰", url="https://t.me/MG_MEDIA") ])       
         # if show_invite is True Append invite link buttons
         if show_invite:
             
