99939f823edb49792ee9d02ef48d3abebc896131
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-05-26 23:19:14 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -128,7 +128,7 @@ async def auto_filter(bot, update):
     else:
         Send_message = await bot.send_message(
             chat_id=update.chat.id,
-            text="Your Content",
+            text="<b>Couldn't Find This Movie.Try Again ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>",
             reply_to_message_id=update.message_id
         )
         await asyncio.sleep(5)
